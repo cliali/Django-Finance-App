b35ec16be8ee5550dae611e22921122ef9111cde
Ali ;) <cliali1181@gmail.com>
2024-11-08 18:48:08 +0330

    🚨 ...

--- config/celery.py
+++ config/celery.py
@@ -5,4 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.dev')
 
 celery = Celery('config')
 celery.config_from_object('django.conf:django', namespace='CELERY')
+celery.conf.broker_connection_retry_on_startup=True
 celery.autodiscover_tasks()
