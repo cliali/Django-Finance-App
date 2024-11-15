1e32e0ae9b9f5825c7c090d96b2075a90e406ed9
Ali ;) <cliali1181@gmail.com>
2024-11-15 21:22:32 +0330

    ✨ add transaction lists

--- finance_config/tracker/urls.py
+++ finance_config/tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "tracker"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('transactions', views.transactions_list, name='transactions-list'),
 ]
--- finance_config/tracker/views.py
+++ finance_config/tracker/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from finance_config.tracker.models import Transaction
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
     return render(request, "tracker/index.html")
+
+
+@login_required
+def transactions_list(request):
+    transaction_filter = Transaction.objects.filter(user=request.user)
+    context = {"filter": transaction_filter}
+    return render(
+        request, "tracker/transactions-list.html", context
+    )
