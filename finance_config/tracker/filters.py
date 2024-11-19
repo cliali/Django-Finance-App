54d550ed12e2c79f4ff7f809bbdf77907c0dacaf
Ali ;) <cliali1181@gmail.com>
2024-11-19 21:49:53 +0330

    ✨ add more filters to template

--- finance_config/tracker/filters.py
+++ finance_config/tracker/filters.py
@@ -1,6 +1,7 @@
 import django_filters
+from django import forms
 
-from .models import Transaction
+from .models import Category, Transaction
 
 
 class TransactionFilter(django_filters.FilterSet):
@@ -11,6 +12,24 @@ class TransactionFilter(django_filters.FilterSet):
         empty_label="Any",
     )
 
+    start_date = django_filters.DateFilter(
+        field_name="date",
+        lookup_expr="gte",
+        widget=forms.DateInput(attrs={"type": "date"}),
+        label="Date From",
+    )
+
+    end_date = django_filters.DateFilter(
+        field_name="date",
+        lookup_expr="lte",
+        widget=forms.DateInput(attrs={"type": "date"}),
+        label="Date To",
+    )
+
+    category = django_filters.ModelMultipleChoiceFilter(
+        queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple()
+    )
+
     class Meta:
         model = Transaction
-        fields = ("transaction_type",)
+        fields = ("transaction_type", "start_date", "end_date", "category")
